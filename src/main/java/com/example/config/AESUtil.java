2b904a935782f4f4011d8507ab80bcac9d9c9bdd
Sebas <Sebas@Sebas-PC>
2023-11-21 05:26:59 -0500

    Actualizacion nueva

--- src/main/java/com/example/config/AESUtil.java
+++ src/main/java/com/example/config/AESUtil.java
@@ -1,12 +1,9 @@
 package com.example.config;
 
-import java.io.IOException;
-import java.io.InputStream;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
-import java.util.Properties;
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
@@ -17,9 +14,6 @@ public class AESUtil {
         0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
         0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F
     };
-
-    private static final String SECRET_KEY_FILE = loadSecretKeyFileLocation();
-
     public static String encriptar(String valor) {
         try {
             IvParameterSpec iv = new IvParameterSpec(INIT_VECTOR);
@@ -48,22 +42,6 @@ public class AESUtil {
             return new String(original);
         } catch (Exception e) {
             e.printStackTrace();
-            return null;
-        }
-    }
-
-    private static String loadSecretKeyFileLocation() {
-        Properties prop = new Properties();
-        try (InputStream input = AESUtil.class.getClassLoader().getResourceAsStream("config.properties")) {
-            if (input == null) {
-                System.out.println("Sorry, unable to find config.properties");
-                return null;
-            }
-
-            prop.load(input);
-            return prop.getProperty("secret.key.file");
-        } catch (IOException ex) {
-            ex.printStackTrace();
             return null;
         }
     }
